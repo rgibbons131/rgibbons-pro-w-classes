a05db3bac3a2cbad5ae9cd8b8fe31f0f6759dd78
Richard Gibbons <richiegibbons99@gmail.com>
2022-04-25 09:12:21 -0600

    grade program

--- wk1-proj2/Program.cs
+++ wk1-proj2/Program.cs
@@ -6,7 +6,27 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-          
+          Console.WriteLine("Input grade percentage as an integer");
+          string ValueInText = Console.ReadLine();
+
+          int percent = int.Parse(ValueInText);
+
+          if (percent >= 90)
+          {
+              Console.WriteLine("Your grade is an A");
+          }
+          else if (percent >= 80)
+          {
+              Console.WriteLine("Your grade is a B");
+          }
+          else if (percent >= 70)
+          {
+              Console.WriteLine("Your grade is a C");
+          }
+          else
+          {
+              Console.WriteLine("Your grade is an F");
+          }
         }
     }
 }
