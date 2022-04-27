b279985c445e176c85ae9d0a60ef978c7431862e
Richard Gibbons <richiegibbons99@gmail.com>
2022-04-27 09:10:34 -0600

    adding new project

---
+++ cse210-student-csharp-empty-starter-main/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Cse210Starter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Write your code here
+        }
+    }
+}
