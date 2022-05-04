325df2d75c17b2314674015bc1ff6429e13377a9
Richard Gibbons <richiegibbons99@gmail.com>
2022-05-04 09:15:06 -0600

    created die class

--- cse210-student-articulate-csharp-main/Unit02/Game/Die.cs
+++ cse210-student-articulate-csharp-main/Unit02/Game/Die.cs
@@ -3,29 +3,44 @@ using System;
 
 namespace Unit02.Game
 {
-    // TODO: Implement the Die class as follows...
-    // 1) Add the class declaration. Use the following class comment.
 
+        // TODO: Implement the Die class as follows...
+        // 1) Add the class declaration. Use the following class comment.
+    public class Die{
         /// <summary>
         /// A small cube with a different number of spots on each of its six sides.
         /// 
         /// The responsibility of Die is to keep track of its currently rolled value and the points its
         /// worth.
         /// </summary> 
+        public int points;
+        public int value;
 
-
-    // 2) Create the class constructor. Use the following method comment.
+        // 2) Create the class constructor. Use the following method comment.
 
         /// <summary>
         /// Constructs a new instance of Die.
         /// </summary>
 
     
-    // 3) Create the Roll() method. Use the following method comment.
-        
+        // 3) Create the Roll() method. Use the following method comment.
+        public void Roll()
+        {
+            points = 0;
+            Random rnd = new Random();
+            value = rnd.Next(1,7);
+            if (value == 1)
+            {
+                points += 100;
+            }
+            else if (value == 5)
+            {
+                points += 50;
+            }
+        }
         /// <summary>
         /// Generates a new random value and calculates the points for the die. Fives are 
         /// worth 50 points, ones are worth 100 points, everything else is worth 0 points.
         /// </summary>
-        
+}        
 }
