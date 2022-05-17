2b0fb75dfc923eaf6237406771bcbad37cf03076
Richard Gibbons <richiegibbons99@gmail.com>
2022-05-17 16:25:49 -0600

    finished seeker

--- cse210-student-articulate-csharp-main/Unit03/Game/Director.cs
+++ cse210-student-articulate-csharp-main/Unit03/Game/Director.cs
@@ -38,7 +38,7 @@ namespace Unit03.Game
         /// </summary>
         private void GetInputs()
         {
-            terminalService.WriteText(hider.location.ToString());
+            //terminalService.WriteText(hider.location.ToString());
             int location = terminalService.ReadNumber("\nEnter a location [1-1000]: ");
             seeker.MoveLocation(location);
         }
--- cse210-student-articulate-csharp-main/Unit03/Game/Seeker.cs
+++ cse210-student-articulate-csharp-main/Unit03/Game/Seeker.cs
@@ -7,34 +7,46 @@ namespace Unit03.Game
 
     // 1) Add the class declaration. Use the following class comment.
 
-        /// <summary>
-        /// <para>The person looking for the Hider.</para>
-        /// <para>
-        /// The responsibility of a Seeker is to keep track of its location.
-        /// </para>
-        /// </summary>
-
+        public class Seeker{
+            /// <summary>
+            /// <para>The person looking for the Hider.</para>
+            /// <para>
+            /// The responsibility of a Seeker is to keep track of its location.
+            /// </para>
+            /// </summary>
+            private TerminalService service = new TerminalService();
+            private int currentLocation;
+        
 
     // 2) Create the class constructor. Use the following method comment.
         
-        /// <summary>
-        /// Constructs a new instance of Seeker.
-        /// </summary>
-       
+        public Seeker()
+        {
+            /// <summary>
+            /// Constructs a new instance of Seeker.
+            /// </summary>
+       }
 
     // 3) Create the GetLocation() method. Use the following method comment.
         
+        public int GetLocation(){
         /// <summary>
         /// Gets the current location.
         /// </summary>
         /// <returns>The current location as an int.</returns>
+        return currentLocation;
+        }
         
 
     // 4) Create the MoveLocation(int location) method. Use the following method comment.
         
+        public void MoveLocation(int location){
         /// <summary>
         /// Moves to the given location.
         /// </summary>
         /// <param name="location">The given location.</param>
+        currentLocation = location;
+        }
+        }
     
 }
